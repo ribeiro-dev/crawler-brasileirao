99f1785e8953b5c1afacafde89b63181be75832e
ribeiro-dev <romilson.2r@gmail.com>
2022-09-18 21:58:16 -0300

    get page and standing table

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import requests
+from bs4 import BeautifulSoup
 
 URL = "https://www.placardefutebol.com.br/brasileirao-serie-a"
 
-page = requests.get(URL)
+response = requests.get(URL)
+soup = BeautifulSoup(response.content, "html.parser")
+
+tabela = soup.select_one("div.league-table")
+rows = tabela.find_all("tr")
