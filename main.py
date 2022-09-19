f7183592520b498b401a7a8cc12146ac43e3d2e1
ribeiro-dev <romilson.2r@gmail.com>
2022-09-19 10:43:07 -0300

    add exception treatment

--- main.py
+++ main.py
@@ -2,35 +2,42 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-URL = "https://www.placardefutebol.com.br/brasileirao-serie-a"
+CAMP = "argentino"
+URL = f"https://www.placardefutebol.com.br/campeonato-{CAMP}"
 
-response = requests.get(URL)
-soup = BeautifulSoup(response.content, "html.parser")
+try:
+    response = requests.get(URL)
+    soup = BeautifulSoup(response.content, "html.parser")
 
-tabela = soup.select_one("div.league-table")
-rows = tabela.find_all("tr")
+    tabela = soup.select_one("div.league-table")
+    rows = tabela.find_all("tr")
 
-# remove o primeiro item, que é o cabeçalho da tabela
-rows.pop(0)
+    # remove o primeiro item, que é o cabeçalho da tabela
+    rows.pop(0)
 
-data = {}
+    data = {}
 
-for index, row in enumerate(rows):
-    tempData = {}
+    for index, row in enumerate(rows):
+        tempData = {}
 
-    texto = row.text.strip() # tira os espaços na lateral
-    team_data = texto.split("\n") # separa os dados
-    
-    tempData["posicao"] = team_data[0]
-    tempData["equipe"] = team_data[1]
-    tempData["pontos"] = team_data[2]
-    tempData["jogos"] = team_data[3]
-    tempData["vitorias"] = team_data[4]
-    tempData["empates"] = team_data[5]
-    tempData["derrotas"] = team_data[6]
-    tempData["saldo_gols"] = team_data[7]
+        texto = row.text.strip() # tira os espaços na lateral
+        team_data = texto.split("\n") # separa os dados
+        
+        tempData["posicao"] = team_data[0]
+        tempData["equipe"] = team_data[1]
+        tempData["pontos"] = team_data[2]
+        tempData["jogos"] = team_data[3]
+        tempData["vitorias"] = team_data[4]
+        tempData["empates"] = team_data[5]
+        tempData["derrotas"] = team_data[6]
+        tempData["saldo_gols"] = team_data[7]
 
-    data[index] = tempData
+        data[index] = tempData
 
-with open("campeonato-brasileiro.json", "w") as file:
-    json_object = json.dump(data, file, indent = 4) 
+    # cria o arquivo json
+    with open(f"campeonato-{CAMP}.json", "w") as file:
+        json_object = json.dump(data, file, indent = 4) 
+
+
+except Exception as err:
+    print("Tivemos um problema..")
